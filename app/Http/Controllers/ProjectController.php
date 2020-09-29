63438c78c76b6a262ad976890157282de78c30c1
Daniel Reales Harana <daniel@MacBook-Pro-de-Daniel.local>
2020-09-29 19:12:55 +0200

    Route Model Binding

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -47,10 +47,10 @@ class ProjectController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Project $project)
     {
         return view('projects.show', [
-            'project' => Project::findOrFail($id)
+            'project' => $project
         ]);
     }
 
--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,6 @@ App::setLocale('es');
 Route::view('/', 'home')->name('home');
 Route::view('/quienes-somos', 'about')->name('about');
 Route::get('/portafolio', [ProjectController::class, 'index'])->name('projects.index');
-Route::get('/portfaolio/{id}', [ProjectController::class, 'show'])->name('projects.show');
+Route::get('/portfaolio/{project}', [ProjectController::class, 'show'])->name('projects.show');
 Route::view('/contacto', 'contact')->name('contact');
 Route::post('contact', [MessageController::class, 'store'])->name('messages.store');
